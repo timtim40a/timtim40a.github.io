d6ea81d9fea6d2089bddb5a8754865de0cbafbcf
timtim40a <timtim40a@gmail.com>
2025-12-17 00:51:05 +0000

    Fix Github logo path

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -5,7 +5,7 @@ function Footer() {
     return (
         <>
             <footer className="footer">
-                <img className="footer__logo" src="/public/github-white-icon.webp"></img>
+                <img className="footer__logo" src="github-white-icon.webp"></img>
                 <p className="footer__text">© 2025 Tymur Soroka. All rights reserved.</p>
             </footer>
         </>
