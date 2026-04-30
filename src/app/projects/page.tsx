fda7eb6b487876e94e96b119f723f5478b1e738c
Tymur Soroka <timtim40a@gmail.com>
2026-04-30 15:47:10 +0100

    Add missing description

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,6 +1,8 @@
 // app/projects/page.tsx — Server Component (no directive)
 import projectsDataRaw from "../../../public/projects/projects.json";
 import ProjectsCarousel from "../_components/projectCarousel/ProjectCarousel"; // client component
+import styles from "./projects.module.css";
+
 
 export default function ProjectsPage() {
     const webProjects = projectsDataRaw.filter(
@@ -12,6 +14,17 @@ export default function ProjectsPage() {
         <>
             <ProjectsCarousel title="Web Development" projects={webProjects} />
             <ProjectsCarousel title="Other Projects" projects={otherProjects} />
+            <p className={styles.description}>
+                I have experience in web development, having created multiple
+                projects using <strong>React</strong>, <strong>Node.js</strong>,
+                and other technologies. These include innovative solutions{" "}
+                <strong>utilising AI</strong>; regular, well structured
+                full-stack web-apps (database management, user authentication,
+                API integration), and quirky creative projects that explore the
+                intersection of technology and art. I love to (and proud to be
+                able to) push the boundaries of what's possible with web
+                technologies.
+            </p>
         </>
     );
 }
